0349cc4eba940a2159e0453170da4bfd403af8bf
kant1992 <631212052@qq.com>
2020-01-30 21:51:51 +0800

    增加项目说明

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -30,6 +30,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
+    'conf',
+    'comment',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
